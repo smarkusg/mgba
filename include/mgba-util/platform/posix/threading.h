10eccccee835cea83cdd091b59b7820a10cf08ad
smarkusg <smarkusg@gmail.com>
2024-05-22 09:17:23 +0200

    init

---
+++ include/mgba-util/platform/posix/threading.h
@@ -0,0 +1,171 @@
+/* Copyright (c) 2013-2015 Jeffrey Pfau
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#ifndef POSIX_THREADING_H
+#define POSIX_THREADING_H
+
+#include <mgba-util/common.h>
+
+
+CXX_GUARD_START
+
+#include <pthread.h>
+#include <time.h>
+#ifdef HAVE_PTHREAD_NP_H
+#include <pthread_np.h>
+#elif defined(__HAIKU__)
+#include <OS.h>
+#endif
+
+#define THREAD_ENTRY void*
+typedef THREAD_ENTRY (*ThreadEntry)(void*);
+
+typedef pthread_t Thread;
+typedef pthread_mutex_t Mutex;
+typedef pthread_cond_t Condition;
+
+static inline int thread_self_set_prio(int prio_level)
+{
+
+    struct sched_param priority;
+
+    if(prio_level > 0)
+    {
+
+        memset(&priority, 0, sizeof(priority));
+        priority.sched_priority = prio_level;
+
+        if(pthread_setschedparam(pthread_self(), SCHED_FIFO, &priority) == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+static inline int MutexInit(Mutex* mutex) {
+	return pthread_mutex_init(mutex, 0);
+}
+
+static inline int MutexDeinit(Mutex* mutex) {
+	return pthread_mutex_destroy(mutex);
+}
+
+static inline int MutexLock(Mutex* mutex) {
+	return pthread_mutex_lock(mutex);
+}
+
+static inline int MutexTryLock(Mutex* mutex) {
+	return pthread_mutex_trylock(mutex);
+}
+
+static inline int MutexUnlock(Mutex* mutex) {
+	return pthread_mutex_unlock(mutex);
+}
+
+static inline int ConditionInit(Condition* cond) {
+	return pthread_cond_init(cond, 0);
+}
+
+static inline int ConditionDeinit(Condition* cond) {
+	return pthread_cond_destroy(cond);
+}
+
+static inline int ConditionWait(Condition* cond, Mutex* mutex) {
+	return pthread_cond_wait(cond, mutex);
+}
+
+static inline int ConditionWaitTimed(Condition* cond, Mutex* mutex, int32_t timeoutMs) {
+	struct timespec ts;
+	struct timeval tv;
+
+	gettimeofday(&tv, 0);
+	ts.tv_sec = tv.tv_sec;
+	ts.tv_nsec = (tv.tv_usec + timeoutMs * 1000L) * 1000L;
+	if (ts.tv_nsec >= 1000000000L) {
+		ts.tv_nsec -= 1000000000L;
+		++ts.tv_sec;
+	}
+
+	return pthread_cond_timedwait(cond, mutex, &ts);
+}
+
+static inline int ConditionWake(Condition* cond) {
+	return pthread_cond_broadcast(cond);
+}
+
+static inline int ThreadCreate(Thread* thread, ThreadEntry entry, void* context) {
+
+pthread_attr_t tattr;
+//pthread_t tid;
+int ret;
+int newprio = 5;
+struct sched_param param;
+
+/* initialized with default attributes */
+ret = pthread_attr_init (&tattr);
+//markus test
+//printf ( "th 1 %i \n",ret );
+
+/* safe to get existing scheduling param */
+ret = pthread_attr_getschedparam (&tattr, &param);
+//printf ( "th 2 %i \n",ret );
+
+/* set the priority; others are unchanged */
+param.sched_priority = newprio;
+
+/* setting the new scheduling param */
+ret = pthread_attr_setschedparam (&tattr, &param);
+//printf ( "th 3 %i \n",ret );
+
+	return pthread_create(thread, &tattr , entry, context);
+//	return pthread_create(thread, 0, entry, context);
+}
+
+static inline int ThreadJoin(Thread* thread) {
+	return pthread_join(*thread, 0);
+}
+
+static inline int ThreadSetName(const char* name) {
+#if defined(__APPLE__) && defined(HAVE_PTHREAD_SETNAME_NP)
+	return pthread_setname_np(name);
+#elif defined(HAVE_PTHREAD_SET_NAME_NP)
+	pthread_set_name_np(pthread_self(), name);
+	return 0;
+#elif defined(__HAIKU__)
+	rename_thread(find_thread(NULL), name);
+	return 0;
+#elif defined(__NetBSD__)
+	return pthread_setname_np(pthread_self(), "%s", (void *) name);
+#elif defined(HAVE_PTHREAD_SETNAME_NP)
+	return pthread_setname_np(pthread_self(), name);
+#else
+//markus
+	thread_self_set_prio (5);
+//
+	UNUSED(name);
+	return 0;
+#endif
+}
+
+#if (__STDC_VERSION__ < 201112L) || (__STDC_NO_THREADS__ == 1)
+typedef pthread_key_t ThreadLocal;
+
+static inline void ThreadLocalInitKey(ThreadLocal* key) {
+	pthread_key_create(key, 0);
+}
+
+static inline void ThreadLocalSetKey(ThreadLocal key, void* value) {
+	pthread_setspecific(key, value);
+}
+
+static inline void* ThreadLocalGetValue(ThreadLocal key) {
+	return pthread_getspecific(key);
+}
+#endif
+
+CXX_GUARD_END
+
+#endif
---
+++ src/platform/sdl/sw-sdl2.c
@@ -0,0 +1,113 @@
+/* Copyright (c) 2013-2015 Jeffrey Pfau
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#include "main.h"
+
+#include <mgba/core/core.h>
+#include <mgba/core/thread.h>
+#include <mgba/core/version.h>
+#include "amigaos.h"
+
+static bool mSDLSWInit(struct mSDLRenderer* renderer);
+static void mSDLSWRunloop(struct mSDLRenderer* renderer, void* user);
+static void mSDLSWDeinit(struct mSDLRenderer* renderer);
+
+void mSDLSWCreate(struct mSDLRenderer* renderer) {
+	renderer->init = mSDLSWInit;
+	renderer->deinit = mSDLSWDeinit;
+	renderer->runloop = mSDLSWRunloop;
+}
+
+bool mSDLSWInit(struct mSDLRenderer* renderer) {
+
+	unsigned width, height;
+
+	renderer->core->baseVideoSize(renderer->core, &width, &height);
+	renderer->window = SDL_CreateWindow("", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, renderer->viewportWidth, renderer->viewportHeight, SDL_WINDOW_RESIZABLE | (SDL_WINDOW_FULLSCREEN * renderer->player.fullscreen));
+	SDLAcaption(renderer->window,renderer->fname);
+	SDL_GetWindowSize(renderer->window, &renderer->viewportWidth, &renderer->viewportHeight);
+	renderer->player.window = renderer->window;
+	renderer->sdlRenderer = SDL_CreateRenderer(renderer->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+        SDL_RenderSetIntegerScale(renderer->sdlRenderer, SDL_FALSE);
+        SDL_RenderSetLogicalSize(renderer->sdlRenderer, renderer->viewportWidth, renderer->viewportHeight);
+        SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
+        SDL_SetHint(SDL_HINT_RENDER_LOGICAL_SIZE_MODE,"overscan");
+
+#ifdef COLOR_16_BIT
+#ifdef COLOR_5_6_5
+	renderer->sdlTex = SDL_CreateTexture(renderer->sdlRenderer, SDL_PIXELFORMAT_RGB565, SDL_TEXTUREACCESS_STREAMING, width, height);
+#else
+	renderer->sdlTex = SDL_CreateTexture(renderer->sdlRenderer, SDL_PIXELFORMAT_ABGR1555, SDL_TEXTUREACCESS_STREAMING, width, height);
+#endif
+#else
+	renderer->sdlTex = SDL_CreateTexture(renderer->sdlRenderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STREAMING, width, height);
+#endif
+
+       SDL_SetWindowMinimumSize(renderer->window, 160, 144);
+        if (renderer->player.fullscreen)
+          SDL_ShowCursor(SDL_FALSE);
+        else SDL_ShowCursor(SDL_TRUE);
+
+	int stride;
+	SDL_LockTexture(renderer->sdlTex, 0, (void**) &renderer->outputBuffer, &stride);
+	renderer->core->setVideoBuffer(renderer->core, renderer->outputBuffer, stride / BYTES_PER_PIXEL);
+
+	return true;
+}
+
+void mSDLSWRunloop(struct mSDLRenderer* renderer, void* user) {
+	struct mCoreThread* context = user;
+	SDL_Event event;
+	bool ARender=SDL_FALSE;
+
+#ifdef __AMIGAOS4__
+	unsigned width,height,g_width,g_height;
+
+	SDL_GetRendererOutputSize(SDL_GetRenderer(renderer->window), &width, &height);
+	SDL_Rect dstRect = {0, 0, width, height};
+        SDL_Rect srcRect = {0, 0, 160, 144};
+
+	renderer->core->desiredVideoDimensions(renderer->core, &g_width, &g_height);
+
+	if ((g_width==160)&&(g_height==144)) {
+	 SDL_SetWindowResizable(renderer->window,SDL_FALSE);
+	 ARender=SDL_TRUE;
+       } 
+#endif
+
+// loop
+	while (mCoreThreadIsActive(context)) {
+
+		while (SDL_PollEvent(&event)) {
+			mSDLHandleEvent(context, &renderer->player, &event);
+		}
+		if (mCoreSyncWaitFrameStart(&context->impl->sync)) {
+
+			SDL_UnlockTexture(renderer->sdlTex);
+#ifdef __AMIGAOS4__
+
+			if (ARender==SDL_TRUE) {
+			    SDL_RenderCopy(renderer->sdlRenderer, renderer->sdlTex, &srcRect, &dstRect);
+			} else {
+			SDL_RenderCopy(renderer->sdlRenderer, renderer->sdlTex, 0, 0);
+			}
+#else
+			SDL_RenderCopy(renderer->sdlRenderer, renderer->sdlTex, 0, 0);
+#endif
+			SDL_RenderPresent(renderer->sdlRenderer);
+			int stride;
+			SDL_LockTexture(renderer->sdlTex, 0, (void**) &renderer->outputBuffer, &stride);
+			renderer->core->setVideoBuffer(renderer->core, renderer->outputBuffer, stride / BYTES_PER_PIXEL);
+		}
+		mCoreSyncWaitFrameEnd(&context->impl->sync);
+	}
+}
+
+void mSDLSWDeinit(struct mSDLRenderer* renderer) {
+	if (renderer->ratio > 1) {
+		free(renderer->outputBuffer);
+	}
+}
